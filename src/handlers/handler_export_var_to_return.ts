ec508d616650304953fac2d8a2cc1edf6e899ce1
vagrant <vagrant@localhost.localdomain>
2021-07-25 01:17:54 +0800

    fix

--- src/handlers/handler_export_var_to_return.ts
+++ src/handlers/handler_export_var_to_return.ts
@@ -21,21 +21,22 @@ export async function export_var_to_return(textEditor: vscode.TextEditor, edit: 
     export_var = export_var.trim();
     let [first_part, middle_part, end_part] = splitFileContent(content)
     let middle_list = convert_middle_part(middle_part)
-    if(middle_list.indexOf(export_var) > -1  ) {
+    if (middle_list.indexOf(export_var) > -1) {
         vscode.window.showErrorMessage(`变量[${export_var}]已经导出`)
         return
     }
+    let ident = " ".repeat(8)
     let new_middle_part_list = [
-        "// __export__",
-        "return {",
+        ident + "// __export__",
+        ident + "return {",
     ]
     middle_list.push(export_var)
-    for(let var_name of middle_list) {
-        new_middle_part_list.push(`${var_name},`)
+    for (let var_name of middle_list) {
+        new_middle_part_list.push(ident + " ".repeat(4) + `${var_name},`)
     }
     new_middle_part_list.push(
-        "}",
-        "// __end_export__"
+        ident + "}",
+        ident + "// __end_export__"
     )
     let new_middle_part = new_middle_part_list.join(os.EOL)
     let new_content = first_part + new_middle_part + end_part
@@ -44,18 +45,18 @@ export async function export_var_to_return(textEditor: vscode.TextEditor, edit: 
 
 }
 
-function convert_middle_part(content:string):Array<string> {
+function convert_middle_part(content: string): Array<string> {
     let out = []
     let lines = content.split(os.EOL)
     let escape_lines = ["// __export__", "// __end_export__", "return {", "}"]
-    for(let line of lines) {
+    for (let line of lines) {
         line = line.trim()
-        if(escape_lines.indexOf(line) > -1) {
+        if (escape_lines.indexOf(line) > -1) {
             continue
         }
         let parts = line.split(",")
-        for(let part of parts) {
-            if(part && part.trim()) {
+        for (let part of parts) {
+            if (part && part.trim()) {
                 out.push(part.trim())
             }
         }
@@ -70,12 +71,13 @@ function splitFileContent(content: string): [string, string, string] {
     let linesChoices = [firstLines, middleLines, endLines]
     let choiceIndex = 0
     for (let line of lines) {
-        line = line.trim()
-        if (line === "// __export__") {
+        // 不push trim_line的原因是未了保持缩进
+        let trim_line = line.trim()
+        if (trim_line === "// __export__") {
             choiceIndex = 1
         }
         linesChoices[choiceIndex].push(line)
-        if (line == "// __end_export__") {
+        if (trim_line == "// __end_export__") {
             choiceIndex = 2
         }
     }
